1a0c5dcb9380e50376f48a82d7ca84c2ca8c880f
YohanAmbinintsoa <yohanrabepro@gmail.com>
2025-03-21 20:18:33 +0300

    Fix backend path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    API_URL: 'https://garagem1back-production.up.railway.app/',
+    API_URL: 'https://garagem1back-production.up.railway.app',
   };
